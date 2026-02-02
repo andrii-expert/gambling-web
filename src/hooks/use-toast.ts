41ecd808df147399365da803df9357ac410a42bd
andrii-expert <andrciganchuk@gmail.com>
2026-02-03 00:58:02 +0100

    complete genId & main actionTypes

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -8,4 +8,25 @@ import type {
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
 
+type ToasterToast = ToastProps & {
+  id: string
+  title?: React.ReactNode
+  description?: React.ReactNode
+  action?: ToastActionElement
+}
+
+const actionTypes = {
+  ADD_TOAST: "ADD_TOAST",
+  UPDATE_TOAST: "UPDATE_TOAST",
+  DISMISS_TOAST: "DISMISS_TOAST",
+  REMOVE_TOAST: "REMOVE_TOAST",
+} as const
+
+let count = 0
+
+function genId() {
+  count = (count + 1) % Number.MAX_SAFE_INTEGER
+  return count.toString()
+}
+
 export { useToast, toast }
