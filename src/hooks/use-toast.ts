6fdbe7d7ea2d19f6068eb3718e9e53a06008afb0
andrii-expert <andrciganchuk@gmail.com>
2026-02-03 01:00:15 +0100

    Define actions
    Co-authored-by: darkhorse512 <taloss6555@gmail.com>
    Co-authored-by: darkhorsebusiness <business220999@gmail.com>

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -29,4 +29,25 @@ function genId() {
   return count.toString()
 }
 
+type ActionType = typeof actionTypes
+
+type Action =
+  | {
+      type: ActionType["ADD_TOAST"]
+      toast: ToasterToast
+    }
+  | {
+      type: ActionType["UPDATE_TOAST"]
+      toast: Partial<ToasterToast>
+    }
+  | {
+      type: ActionType["DISMISS_TOAST"]
+      toastId?: ToasterToast["id"]
+    }
+  | {
+      type: ActionType["REMOVE_TOAST"]
+      toastId?: ToasterToast["id"]
+    }
+
+
 export { useToast, toast }
